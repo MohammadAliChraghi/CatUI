0dca918e118ec72faa25467ae591a933c6e604c8
MohammadAliChraghi <mac85127@gmail.com>
2021-01-04 17:45:24 +0330

    Changed Sidenav Close Method

--- src/script.js
+++ src/script.js
@@ -6,3 +6,25 @@ $(".opensidenav").on("click", function () {
     300
   );
 });
+/*
+$(document).click(function (e) {
+  if (!$(e.target).is(".sidenav") && !$(e.target).is(".opensidenav")) {
+    $(".sidenav").css("width", "0");
+  }
+});
+*/
+
+$(window).click(function () {
+  if (parseInt($(".sidenav").css("width")) > 0) {
+    $(".sidenav").animate(
+      {
+        width: "0",
+      },
+      300
+    );
+  }
+});
+
+$(".sidenav").click(function (event) {
+  event.stopPropagation();
+});
