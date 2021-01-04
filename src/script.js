28c23be04bf40b48031c96792547db4a6b3a8384
MohammadAliChraghi <mac85127@gmail.com>
2021-01-04 22:01:08 +0330

    Sidenav Bugs Fixed

--- src/script.js
+++ src/script.js
@@ -1,30 +1,21 @@
-$(".opensidenav").on("click", function () {
-  $(".sidenav").animate(
-    {
-      width: "400px",
-    },
-    300
-  );
-});
-/*
-$(document).click(function (e) {
-  if (!$(e.target).is(".sidenav") && !$(e.target).is(".opensidenav")) {
-    $(".sidenav").css("width", "0");
-  }
-});
-*/
-
-$(window).click(function () {
-  if (parseInt($(".sidenav").css("width")) > 0) {
-    $(".sidenav").animate(
-      {
-        width: "0",
-      },
-      300
-    );
+$("[data-open-sidenav]").on("click", function() {
+  if ($(this).data("open-sidenav").length && $(`#${$(this).data("open-sidenav")}`).length) {
+    $(`#${$(this).data("open-sidenav")}`).animate({
+      left: "0"
+    }, 300);
   }
 });
 
-$(".sidenav").click(function (event) {
+$(window).on("click", function() {
+  $('.sidenav').each(function() {
+    if (parseInt($(this).css("left")) === 0) {
+      $(this).animate({
+        left: "-400px"
+      }, 300, "linear");
+    }
+  });
+});
+
+$(".sidenav").on("click", function(event) {
   event.stopPropagation();
 });
