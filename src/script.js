0785376111d934024351df5b6c58d2cb4352a07d
MohammadAliChraghi <mac85127@gmail.com>
2021-01-06 15:12:36 +0330

    Update Dialog

--- src/script.js
+++ src/script.js
@@ -5,6 +5,44 @@ $("[data-open]").on("click", function () {
   }
 });
 
+$("[data-close]").on("click", function () {
+  var e = $(this).data("close");
+  if (e.length) {
+    if ($(`#${e}`).length) {
+      var specialEls = ["dialog"];
+      var ElType;
+      function isSpecial() {
+        for (i in specialEls) {
+          if (
+            $(`#${e}`).hasClass(specialEls[i]) ||
+            $(`#${e}`).prop("tagName").toLowerCase() === specialEls[i]
+          ) {
+            ElType = specialEls[i];
+            return true;
+          } else {
+            return false;
+          }
+          break;
+        }
+      }
+      // Specials
+      if (isSpecial()) {
+        if (ElType === "dialog") {
+          $(`#${e}`)
+            .css("box-shadow", "0 0 0 0 rgba(0,0,0,0.4)")
+            .animate({ bottom: "-120vh" });
+        }
+      }
+      // Specials
+    }
+    else{
+      console.error("CatUI Error Code 1 : Target Element Not Found!");
+    }
+  } else {
+    $(this).parent().addClass("d-none");
+  }
+});
+
 $(window).on("click", function () {
   $(".sidenav, sidenav").each(function () {
     var str = $(this).css("transform");
